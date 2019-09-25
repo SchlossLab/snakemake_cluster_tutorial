9860a0a8c21c6e907c362a57f368876f0937c066
William L. Close <close.will@gmail.com>
2019-09-25 12:38:35 -0400

    adding slurm submission script

---
+++ code/clusterSnakemake.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=clusterSnakemake
+
+# Resources
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=1000mb
+#SBATCH --time=48:00:00
+
+# Account
+#SBATCH --account=ACCOUNT
+#SBATCH --partition=standard
+
+# Logs
+#SBATCH --mail-user=EMAIL
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=code/log/%x-%j.out
+
+# Environment
+##SBATCH --export=ALL
+
+# List compute nodes allocated to the job
+if [[ $SLURM_JOB_NODELIST ]] ; then
+	echo "Running on"
+	scontrol show hostnames $SLURM_JOB_NODELIST
+	echo -e "\n"
+fi
+
+
+
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
+
+# Initiating snakemake and running workflow in cluster mode
+snakemake --profile config/slurm/ --latency-wait 20
